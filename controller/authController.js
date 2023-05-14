fe2898436cdd87a3e53041dc7864440433f246ba
ankurksoni <ankursoni.leonardo@gmail.com>
2023-05-14 15:05:52 +0530

    chores: models, login post flow, executions.

--- controller/authController.js
+++ controller/authController.js
@@ -1,9 +1,18 @@
+const User = require('../models/User');
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
 
-module.exports.signup_post = (req, res) => {
-    res.send('signup');
+module.exports.signup_post = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.create({ email, password });
+        res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send('Error, cannot create User.');
+    }
 }
 
 module.exports.login_get = (req, res) => {
