ecdcf5f461ab6e1c211188f1f20ebb07e248092a
ankurksoni <ankursoni.leonardo@gmail.com>
2023-05-15 14:02:25 +0530

    chores: added compression pkg.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
 const { requireAuth, checkUser } = require('./middleware/authMiddleware');
-
+const compression = require('compression');
 
 const app = express();
 const PORT = 3000;
@@ -13,6 +13,7 @@ const dbURI = 'mongodb://mongo:27017/node-auth';
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
+app.use(compression());
 
 // view engine
 app.set('view engine', 'ejs');
